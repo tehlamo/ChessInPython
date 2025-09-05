ff3568abec0924b0b59f5fd49c08e41e9c9db4b4
DonovanThach <tehlamo0528@gmail.com>
2025-09-04 17:46:34 -0700

    little change

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ program = True
 
 def play_game():
     game = ChessGame()
-    print("Welcome to Chess! Type moves like 'g1 e1' to move pieces. To check the board, type 'get_game.' Type 'get_game_state' to see the game state. Type 'get_current_turn' to see whose turn it is. Type 'quit' to quit. Type 'force_quit' to exit the program.")
+    print("Welcome to Chess! Type moves like 'g1 e1' to move pieces. To check the board, type 'get_game.' Type 'get_game_state' to see the game state. Type 'get_current_turn' to see whose turn it is. Type 'quit' to quit.")
     while True:
         user_input = input("Enter command: ").strip().lower()
         if user_input == 'quit':
@@ -19,7 +19,7 @@ def play_game():
             game.get_current_turn()
 
 while program is True:
-    print("To start a new game, type 'new.'")
+    print("To start a new game, type 'new.' To exit the program, type 'force_quit.'")
     user_input = input("Enter Command: ")
     if user_input == 'new':
         play_game()
@@ -27,4 +27,4 @@ while program is True:
         program = False
         break
     else:
-        print("Type 'new' to start the game.")
+        print("Type 'new' to start the game. Type 'force_quit' to exit the program.")
