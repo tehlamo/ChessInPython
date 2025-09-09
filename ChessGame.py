dbc1fc18b9d599efb069affec36f6bcf8da60aeb
DonovanThach <tehlamo0528@gmail.com>
2025-09-08 20:05:02 -0700

    why are pawns so complicated bro i just wanna do en passant in peace

--- ChessGame.py
+++ ChessGame.py
@@ -3,18 +3,18 @@ from PiecesForGame import Piece, Pawn, Rook, Knight, Bishop, Queen, King
 class ChessGame:
     def __init__(self):
         self._board = []
-        for row in range(8):
-            col = [None, None, None, None, None, None, None, None]
-            self._board.append(col)
+        for col in range(8):
+            row = [None, None, None, None, None, None, None, None]
+            self._board.append(row)
         self._game_state = 'UNFINISHED'
         self._current_turn = 'WHITE'
         self._sp_starting_row = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
         self._pawn_starting_row = [Pawn]*8
-        for row in range(8):
-            self._board[0][row] = self._sp_starting_row[row]('BLACK')
-            self._board[1][row] = self._pawn_starting_row[row]('BLACK')
-            self._board[6][row] = self._pawn_starting_row[row]('WHITE')
-            self._board[7][row] = self._sp_starting_row[row]('WHITE')
+        for col in range(8):
+            self._board[0][col] = self._sp_starting_row[col]('BLACK')
+            self._board[1][col] = self._pawn_starting_row[col]('BLACK')
+            self._board[6][col] = self._pawn_starting_row[col]('WHITE')
+            self._board[7][col] = self._sp_starting_row[col]('WHITE')
 
     def get_game_state(self):
         return self._game_state
@@ -23,15 +23,15 @@ class ChessGame:
         return self._current_turn
     
     def get_game(self):
-        for row in range(len(self._board)):
+        for col in range(len(self._board)):
             symbols = []
-            for col in range(len(self._board[row])):
-                if self._board[row][col] is None:
+            for row in range(len(self._board[col])):
+                if self._board[col][row] is None:
                     symbols.append("□")
                 else:
-                    symbols.append(self._board[row][col].get_symbol())
-            symbol_row = " ".join(symbols)
-            print(symbol_row)
+                    symbols.append(self._board[col][row].get_symbol())
+            symbol_col = " ".join(symbols)
+            print(symbol_col)
 
     def make_move(self, from_sq, to_sq):
         from_coord = self._get_indicies(from_sq)
@@ -41,17 +41,17 @@ class ChessGame:
         elif to_coord is None:
             return False
         else:
-            from_col, from_row = from_coord
+            from_row, from_col = from_coord
             to_col, to_row = to_coord
-            if self._board[from_col][from_row].get_color() != self._current_turn:
+            if self._board[from_row][from_col].get_color() != self._current_turn:
                 print("Invalid move, it is currently", self._current_turn, "player's turn.")
-            elif self._board[from_col][from_row] is None:
+            elif self._board[from_row][from_col] is None:
                 print("Invalid move, piece to move doesn't exist.")
 
 
     def _get_indicies(self, coordinate):
-        letter = coordinate[0]
-        col = ord(letter) - ord('a')
         number = int(coordinate[1])
         row = number - 1
-        return col, row
+        letter = coordinate[0]
+        col = ord(letter) - ord('a')
+        return row, col
--- PiecesForGame.py
+++ PiecesForGame.py
@@ -31,32 +31,32 @@ class Rook(Piece):
         elif self._color == 'WHITE':
             self._symbol = '♜'
 
-    def is_valid_move(self, from_col, from_row, to_col, to_row, board):
-        delta_col = to_col - from_col
+    def is_valid_move(self, from_row, from_col, to_row, to_col, board):
         delta_row = to_row - from_row
-        temp_col = from_col
+        delta_col = to_col - from_col
         temp_row = from_row
-        if delta_col > 0:
-            step_col = 1
-        elif delta_col < 0:
-            step_col = -1
-        else:
-            step_col = 0
+        temp_col = from_col
         if delta_row > 0:
             step_row = 1
         elif delta_row < 0:
             step_row = -1
         else:
             step_row = 0
+        if delta_col > 0:
+            step_col = 1
+        elif delta_col < 0:
+            step_col = -1
+        else:
+            step_col = 0
 
-        if ((abs(delta_col) == 0) or (abs(delta_row) == 0)) and (1 <= abs(delta_col) + abs(delta_row)):
-            for square in range(abs(delta_col) + abs(delta_row) - 1):
-                temp_col = temp_col + step_col
+        if ((abs(delta_row) == 0) or (abs(delta_col) == 0)) and (1 <= abs(delta_row) + abs(delta_col)):
+            for square in range(abs(delta_row) + abs(delta_col) - 1):
                 temp_row = temp_row + step_row
-                if board[temp_col][temp_row] is not None and temp_col != to_col and temp_row != to_row:
+                temp_col = temp_col + step_col
+                if (board[temp_row][temp_col] is not None) and (temp_row != to_row and temp_col != to_col):
                     return False
-                elif board[temp_col][temp_row] is not None and temp_col == to_col and temp_row == to_row:
-                    if board[temp_col][temp_row].get_color() == self._color:
+                elif (board[temp_row][temp_col] is not None) and (temp_row == to_row and temp_col == to_col):
+                    if board[temp_row][temp_col].get_color() == self._color:
                         return False
                     else:
                         return True
@@ -73,6 +73,62 @@ class Knight(Piece):
         elif self._color == 'WHITE':
             self._symbol = '♞'
 
+    def is_valid_move(self, from_row, from_col, to_row, to_col, board):
+        delta_row = to_row - from_row
+        delta_col = to_col - from_col
+        temp_row = from_row
+        temp_col = from_col
+        if delta_row > 0:
+            step_row = 1
+        elif delta_row < 0:
+            step_row = -1
+        else:
+            step_row = 0
+        if delta_col > 0:
+            step_col = 1
+        elif delta_col < 0:
+            step_col = -1
+        else:
+            step_col = 0
+
+        if (self._color == 'BLACK') and (from_row == 2) and (1 == abs(delta_row) or abs(delta_row) == 2) and (delta_row < 0):
+            for square in range(abs(delta_row + delta_col) - 1):
+                temp_row = temp_row + step_row
+                temp_col = temp_col + step_col
+                if (board[temp_row][temp_col] is not None) and (temp_row != to_row and temp_col != to_col):
+                    return False
+                elif (board[temp_row][temp_col] is not None) and (temp_row == to_row and temp_col == to_col):
+                    if board[temp_row][temp_col].get_color() == self._color:
+                        return False
+                    else:
+                        return True
+            return True
+        elif (self._color == 'WHITE') and (from_row == 7) and (abs(delta_row) == 1 or abs(delta_row) == 2) and (delta_row > 0):
+            for square in range(abs(delta_row + delta_col) - 1):
+                temp_row = temp_row + step_row
+                temp_col = temp_col + step_col
+                if (board[temp_row][temp_col] is not None) and (temp_row != to_row and temp_col != to_col):
+                    return False
+                elif (board[temp_row][temp_col] is not None) and (temp_row == to_row and temp_col == to_col):
+                    if board[temp_row][temp_col].get_color() == self._color:
+                        return False
+                    else:
+                        return True
+            return True
+        elif (self._color == 'BLACK') and (from_row != 2 and from_row != 5) and (abs(delta_row) == 1) and (delta_row < 0):
+            if (board[to_row][to_col] is not None) and (board[temp_row][temp_col].get_color() == self._color):
+                return False
+            else:
+                return True
+        elif  (self._color == 'WHITE') and (from_row != 7 and from_row != 4) and (abs(delta_row) == 1) and (delta_row > 0):
+            if (board[to_row][to_col] is not None) and (board[temp_row][temp_col].get_color() == self._color):
+                return False
+            else:
+                return True
+        elif (self._color == 'BLACK') and (from_row == 5):
+            if (board[from_row][from_col + 1].get_name() == 'Pawn') and (delta_row > 0) and (abs(delta_row) == abs(delta_col)):
+                
+
 class Bishop(Piece):
     def __init__(self, color):
         super().__init__(color)
@@ -81,6 +137,39 @@ class Bishop(Piece):
             self._symbol = '♗'
         elif self._color == 'WHITE':
             self._symbol = '♝'
+
+    def is_valid_move(self, from_row, from_col, to_row, to_col, board):
+        delta_row = to_row - from_row
+        delta_col = to_col - from_col
+        temp_row = from_row
+        temp_col = from_col
+        if delta_row > 0:
+            step_row = 1
+        elif delta_row < 0:
+            step_row = -1
+        else:
+            step_row = 0
+        if delta_col > 0:
+            step_col = 1
+        elif delta_col < 0:
+            step_col = -1
+        else:
+            step_col = 0
+
+        if (abs(delta_row) == abs(delta_col)) and (1 <= abs(delta_row)):
+            for square in range(abs(delta_row) - 1):
+                temp_row = temp_row + step_row
+                temp_col = temp_col + step_col
+                if (board[temp_row][temp_col] is not None) and (temp_row != to_row and temp_col != to_col):
+                    return False
+                elif (board[temp_row][temp_col] is not None) and (temp_row == to_row and temp_col == to_col):
+                    if board[temp_row][temp_col].get_color() == self._color:
+                        return False
+                    else:
+                        return True
+            return True
+        else:
+            return False
 
 class Queen(Piece):
     def __init__(self, color):
@@ -91,6 +180,51 @@ class Queen(Piece):
         elif self._color == 'WHITE':
             self._symbol = '♛'
 
+    def is_valid_move(self, from_row, from_col, to_row, to_col, board):
+        delta_row = to_row - from_row
+        delta_col = to_col - from_col
+        temp_row = from_row
+        temp_col = from_col
+        if delta_row > 0:
+            step_row = 1
+        elif delta_row < 0:
+            step_row = -1
+        else:
+            step_row = 0
+        if delta_col > 0:
+            step_col = 1
+        elif delta_col < 0:
+            step_col = -1
+        else:
+            step_col = 0
+
+        if (abs(delta_row) == 0 or abs(delta_col) == 0) and (1 <= abs(delta_row) + abs(delta_col)):
+            for square in range(abs(delta_row) + abs(delta_col)):
+                temp_row = temp_row + step_row
+                temp_col = temp_col + step_col
+                if (board[temp_row][temp_col] is not None) and (temp_row != to_row and temp_col != to_col):
+                    return False
+                elif (board[temp_row][temp_col] is not None) and (temp_row == to_row and temp_col == to_col):
+                    if board[temp_row][temp_col].get_color() == self._color:
+                        return False
+                    else:
+                        return True
+            return True
+        elif (abs(delta_row) == abs(delta_col)) and (1 <= abs(delta_row)):
+            for square in range(abs(delta_col) - 1):
+                temp_row = temp_row + step_row
+                temp_col = temp_col + step_col
+                if (board[temp_row][temp_col] is not None) and (temp_row != to_row and temp_col != to_col):
+                    return False
+                elif (board[temp_row][temp_col] is not None) and (temp_row == to_row and temp_col == to_col):
+                    if board[temp_row][temp_col].get_color() == self._color:
+                        return False
+                    else:
+                        return True
+            return True
+        else:
+            return False
+
 class King(Piece):
     def __init__(self, color):
         super().__init__(color)
@@ -99,3 +233,48 @@ class King(Piece):
             self._symbol = '♔'
         elif self._color == 'WHITE':
             self._symbol = '♚'
+
+    def is_valid_move(self, from_row, from_col, to_row, to_col, board):
+        delta_row = to_row - from_row
+        delta_col = to_col - from_col
+        temp_row = from_row
+        temp_col = from_col
+        if delta_row > 0:
+            step_row = 1
+        elif delta_row < 0:
+            step_row = -1
+        else:
+            step_row = 0
+        if delta_col > 0:
+            step_col = 1
+        elif delta_col < 0:
+            step_col = -1
+        else:
+            step_col = 0
+
+        if (abs(delta_row) == 0 or abs(delta_col) == 0) and (1 == abs(delta_row) + abs(delta_col)):
+            for square in range(abs(delta_row) + abs(delta_col)):
+                temp_row = temp_row + step_row
+                temp_col = temp_col + step_col
+                if (board[temp_row][temp_col] is not None) and (temp_row != to_row and temp_col != to_col):
+                    return False
+                elif (board[temp_row][temp_col] is not None) and (temp_row == to_row and temp_col == to_col):
+                    if board[temp_row][temp_col].get_color() == self._color:
+                        return False
+                    else:
+                        return True
+            return True
+        elif (abs(delta_row) == abs(delta_col)) and (1 == abs(delta_row)):
+            for square in range(abs(delta_col) - 1):
+                temp_row = temp_row + step_row
+                temp_col = temp_col + step_col
+                if (board[temp_row][temp_col] is not None) and (temp_row != to_row and temp_col != to_col):
+                    return False
+                elif (board[temp_row][temp_col] is not None) and (temp_row == to_row and temp_col == to_col):
+                    if board[temp_row][temp_col].get_color() == self._color:
+                        return False
+                    else:
+                        return True
+            return True
+        else:
+            return False
